a2b78e69f1601ea700ecf8fcdd1a868ca7089625
Sicko699 <francesco.mattone.03@gmail.com>
2024-04-06 19:35:05 +0200

    Implementazione login

---
+++ build/LoginApp.py
@@ -0,0 +1,162 @@
+from tkinter import Tk, Canvas, Entry, Button, PhotoImage, messagebox
+from pathlib import Path
+import json
+from MainApp import MainApp
+
+class LoginApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry("862x519")
+        self.master.configure(bg="#FAFFFD")
+
+        self.canvas = Canvas(
+            self.master,
+            bg="#FAFFFD",
+            height=519,
+            width=862,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
+        )
+        self.canvas.place(x=0, y=0)
+
+        self.canvas.create_rectangle(
+            262.0,
+            106.0,
+            599.0,
+            413.0,
+            fill="#56AAFF",
+            outline="")
+
+        self.entry_image_1 = PhotoImage(file=self.relative_to_assets("entry_1.png"))
+        self.entry_bg_1 = self.canvas.create_image(
+            431.5,
+            191.0,
+            image=self.entry_image_1
+        )
+        self.entry_1 = Entry(
+            bd=0,
+            bg="#FAFFFD",
+            fg="#000716",
+            highlightthickness=0
+        )
+        self.entry_1.place(
+            x=315.0,
+            y=172.0,
+            width=233.0,
+            height=36.0
+        )
+
+        self.entry_image_2 = PhotoImage(file=self.relative_to_assets("entry_2.png"))
+        self.entry_bg_2 = self.canvas.create_image(
+            431.5,
+            269.5,
+            image=self.entry_image_2
+        )
+        self.entry_2 = Entry(
+            bd=0,
+            bg="#FAFFFD",
+            fg="#000716",
+            highlightthickness=0,
+            show="*"
+        )
+        self.entry_2.place(
+            x=315.0,
+            y=250.0,
+            width=233.0,
+            height=37.0
+        )
+
+        self.canvas.create_text(
+            303.0,
+            147.0,
+            anchor="nw",
+            text="Username",
+            fill="#FFFFFF",
+            font=("Inter Bold", 18)
+        )
+
+        self.canvas.create_text(
+            303.0,
+            226.0,
+            anchor="nw",
+            text="Password",
+            fill="#FFFFFF",
+            font=("Inter Bold", 18)
+        )
+
+        self.button_image_1 = PhotoImage(file=self.relative_to_assets("button_1.png"))
+        self.button_1 = Button(
+            image=self.button_image_1,
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.login_clicked,
+            relief="flat"
+        )
+        self.button_1.place(
+            x=358.91162109375,
+            y=329.33056640625,
+            width=143.8626708984375,
+            height=38.523895263671875
+        )
+        
+        self.button_image_2 = PhotoImage(file=self.relative_to_assets("button_2.png"))
+        
+        self.button_2 = Button(
+            image=self.button_image_2,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_2 clicked"),
+            relief="flat"
+        )
+        self.button_2.place(
+            x=303.0,
+            y=288.0,
+            width=257.0,
+            height=39.0
+        )
+
+
+        self.master.resizable(False, False)
+
+    def relative_to_assets(self, path: str) -> Path:
+        return ASSETS_PATH / Path(path)
+
+    def verify_login(self, username, password):
+        with open("data.json", "r") as file:
+            data = json.load(file)
+
+        users = data["users"]
+
+        for user in users:
+            if user["username"] == username and user["password"] == password and user["role"] == "admin":
+                return "admin"
+            elif user["username"] == username and user["password"] == password and user["role"] == "utente":
+                return "utente"
+
+        return False
+
+    def login_clicked(self):
+        username = self.entry_1.get()
+        password = self.entry_2.get()
+
+        if self.verify_login(username, password) == "admin":
+            print("Accesso consentito come admin")
+            self.master.destroy()  # Chiude la finestra della LoginApp
+            root = Tk()  # Crea una nuova finestra Tk per la MainApp
+            app = MainApp(root)  # Avvia la MainApp nella nuova finestra Tk
+            root.mainloop()  # Avvia il loop principale della nuova finestra Tk
+        elif self.verify_login(username, password) == "utente":
+            print("Accesso consentito come utente")
+        else:
+            messagebox.showerror("Login Failed", "Invalid username or password")
+
+
+if __name__ == "__main__":
+    OUTPUT_PATH = Path(__file__).parent
+    ASSETS_PATH = OUTPUT_PATH / Path(r"/Users/francescomattone/Desktop/coding/ing_soft2/build/assets/frame3")
+
+    root = Tk()
+    app = LoginApp(root)
+    root.mainloop()
+
---
+++ build/MainApp.py
@@ -0,0 +1,169 @@
+from pathlib import Path
+from tkinter import Tk, Canvas, Button, PhotoImage
+
+
+class MainApp:
+    def __init__(self, window):
+        self.window = window
+        self.window.geometry("862x519")
+        self.window.configure(bg="#FAFFFD")
+
+        self.canvas = Canvas(
+            self.window,
+            bg="#FAFFFD",
+            height=519,
+            width=862,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
+        )
+        
+        self.canvas.place(x=0, y=0)
+        self.canvas.create_rectangle(
+            0.0,
+            0.0,
+            210.0,
+            519.0,
+            fill="#3E97F1",
+            outline=""
+        )
+
+        # Load button images and store them as attributes
+        self.button_images = {
+            f"button_{i}": self.load_button_image(f"button_{i}.png") for i in range(1, 13)
+        }
+
+        self.create_buttons()
+
+        self.window.resizable(False, False)
+
+    def create_buttons(self):
+        button_1 = Button(
+            self.window,
+            image=self.button_images["button_1"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_1 clicked"),
+            relief="flat"
+        )
+        button_1.place(x=289.0, y=75.0, width=221.0, height=166.0)
+
+        button_2 = Button(
+            self.window,
+            image=self.button_images["button_2"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_2 clicked"),
+            relief="flat"
+        )
+        button_2.place(x=562.0, y=75.0, width=221.0, height=166.0)
+
+        button_3 = Button(
+            self.window,
+            image=self.button_images["button_3"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_3 clicked"),
+            relief="flat"
+        )
+        button_3.place(x=289.0, y=277.0, width=221.0, height=166.0)
+
+        button_4 = Button(
+            self.window,
+            image=self.button_images["button_4"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_4 clicked"),
+            relief="flat"
+        )
+        button_4.place(x=562.0, y=277.0, width=221.0, height=166.0)
+
+        button_5 = Button(
+            self.window,
+            image=self.button_images["button_5"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_5 clicked"),
+            relief="flat"
+        )
+        button_5.place(x=46.0, y=452.0, width=47.0, height=45.0)
+
+        button_6 = Button(
+            self.window,
+            image=self.button_images["button_6"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_6 clicked"),
+            relief="flat"
+        )
+        button_6.place(x=116.0, y=452.0, width=47.0, height=45.0)
+
+        button_7 = Button(
+            self.window,
+            image=self.button_images["button_7"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_7 clicked"),
+            relief="flat"
+        )
+        button_7.place(x=24.0, y=75.0, width=162.0, height=45.0)
+
+        button_8 = Button(
+            self.window,
+            image=self.button_images["button_8"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_8 clicked"),
+            relief="flat"
+        )
+        button_8.place(x=24.0, y=135.0, width=162.0, height=45.0)
+
+        button_9 = Button(
+            self.window,
+            image=self.button_images["button_9"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_9 clicked"),
+            relief="flat"
+        )
+        button_9.place(x=24.0, y=195.0, width=162.0, height=45.0)
+
+        button_10 = Button(
+            self.window,
+            image=self.button_images["button_10"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_10 clicked"),
+            relief="flat"
+        )
+        button_10.place(x=24.0, y=255.0, width=162.0, height=45.0)
+
+        button_11 = Button(
+            self.window,
+            image=self.button_images["button_11"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_11 clicked"),
+            relief="flat"
+        )
+        button_11.place(x=24.0, y=315.0, width=162.0, height=45.0)
+
+        button_12 = Button(
+            self.window,
+            image=self.button_images["button_12"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_12 clicked"),
+            relief="flat"
+        )
+        button_12.place(x=24.0, y=375.0, width=162.0, height=45.0)
+
+    def load_button_image(self, image_path):
+        ASSETS_PATH = Path("/Users/francescomattone/Desktop/coding/ing_soft2/build/assets/frame2")
+        return PhotoImage(file=ASSETS_PATH / Path(image_path))
+
+
+if __name__ == "__main__":
+    root = Tk()
+    app = MainApp(root)
+    root.mainloop()
