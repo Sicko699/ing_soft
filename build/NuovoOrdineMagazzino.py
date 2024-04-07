dc69f20d8c248720b62e2272517582c85a19c041
Sapo1175 <francesco1175737573@outlook.it>
2024-04-07 16:01:00 +0200

    Modifica gui12, 13

---
+++ build/NuovoOrdineMagazzino.py
@@ -0,0 +1,242 @@
+from pathlib import Path
+import os, platform
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+
+class NuovoOrdineMagazzino:
+    def __init__(self,window):
+        self.window = window
+        self.window.geometry("862x519")
+        self.window.configure(bg = "#FAFFFD")
+
+        self.canvas = Canvas(
+            self.window,
+            bg = "#FAFFFD",
+            height = 519,
+            width = 862,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge"
+        )
+
+        self.canvas.place(x = 0, y = 0)
+        self.canvas.create_rectangle(
+            0.0,
+            0.0,
+            210.0,
+            519.0,
+            fill="#3E97F1",
+            outline=""
+        )
+
+        self.button_image_1 = PhotoImage(file=self.relative_to_assets("button_1.png"))
+        self.button_1 = Button(
+            image=self.button_image_1,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_1 clicked"),
+            relief="flat"
+        )
+        self.button_1.place(
+            x=46.0,
+            y=452.0,
+            width=47.0,
+            height=45.0
+        )
+
+        self.button_image_2 = PhotoImage(file=self.relative_to_assets("button_2.png"))
+        self.button_2 = Button(
+            image=self.button_image_2,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_2 clicked"),
+            relief="flat"
+        )
+        self.button_2.place(
+            x=116.0,
+            y=452.0,
+            width=47.0,
+            height=45.0
+        )
+
+        self.button_image_3 = PhotoImage(file=self.relative_to_assets("button_3.png"))
+        self.button_3 = Button(
+            image=self.button_image_3,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_3 clicked"),
+            relief="flat"
+        )
+        self.button_3.place(
+            x=24.0,
+            y=75.0,
+            width=162.0,
+            height=45.0
+        )
+
+        self.button_image_4 = PhotoImage(file=self.relative_to_assets("button_4.png"))
+        self.button_4 = Button(
+            image=self.button_image_4,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_4 clicked"),
+            relief="flat"
+        )
+        self.button_4.place(
+            x=24.0,
+            y=135.0,
+            width=162.0,
+            height=45.0
+        )
+
+        self.button_image_5 = PhotoImage(file=self.relative_to_assets("button_5.png"))
+        self.button_5 = Button(
+            image=self.button_image_5,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_5 clicked"),
+            relief="flat"
+        )
+        self.button_5.place(
+            x=24.0,
+            y=195.0,
+            width=162.0,
+            height=45.0
+        )
+
+        self.button_image_6 = PhotoImage(file=self.relative_to_assets("button_6.png"))
+        self.button_6 = Button(
+            image=self.button_image_6,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_6 clicked"),
+            relief="flat"
+        )
+        self.button_6.place(
+            x=24.0,
+            y=255.0,
+            width=162.0,
+            height=45.0
+        )
+
+        self.button_image_7 = PhotoImage(file=self.relative_to_assets("button_7.png"))
+        self.button_7 = Button(
+            image=self.button_image_7,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_7 clicked"),
+            relief="flat"
+        )
+        self.button_7.place(
+            x=24.0,
+            y=315.0,
+            width=162.0,
+            height=45.0
+        )
+
+        self.button_image_8 = PhotoImage(file=self.relative_to_assets("button_8.png"))
+        self.button_8 = Button(
+            image=self.button_image_8,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_8 clicked"),
+            relief="flat"
+        )
+        self.button_8.place(
+            x=24.0,
+            y=375.0,
+            width=162.0,
+            height=45.0
+        )
+
+        self.canvas.create_rectangle(
+            344.0,
+            110.0,
+            734.0,
+            410.0,
+            fill="#FAFFFD",
+            outline=""
+        )
+
+        self.entry_image_1 = PhotoImage(file=self.relative_to_assets("entry_1.png"))
+        self.entry_bg_1 = self.canvas.create_image(
+            539.0,
+            185.5,
+            image=self.entry_image_1
+        )
+        self.entry_1 = Entry(
+            bd=0,
+            bg="#EAEEEC",
+            fg="#000716",
+            highlightthickness=0
+        )
+        self.entry_1.place(
+            x=401.0,
+            y=166.0,
+            width=276.0,
+            height=37.0
+        )
+
+        self.entry_image_2 = PhotoImage(file=self.relative_to_assets("entry_2.png"))
+        self.entry_bg_2 = self.canvas.create_image(
+            539.0,
+            260.0,
+            image=self.entry_image_2
+        )
+        self.entry_2 = Entry(
+            bd=0,
+            bg="#EAEEEC",
+            fg="#000716",
+            highlightthickness=0
+        )
+        self.entry_2.place(
+            x=401.0,
+            y=241.0,
+            width=276.0,
+            height=36.0
+        )
+
+        self.canvas.create_text(
+            389.0,
+            146.0,
+            anchor="nw",
+            text="Nome articolo",
+            fill="#000000",
+            font=("Quicksand Medium", 16 * -1)
+        )
+
+        self.canvas.create_text(
+            389.0,
+            221.0,
+            anchor="nw",
+            text="Quantità",
+            fill="#000000",
+            font=("Quicksand Medium", 16 * -1)
+        )
+
+        self.button_image_9 = PhotoImage(file=self.relative_to_assets("button_9.png"))
+        self.button_9 = Button(
+            image=self.button_image_9,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_9 clicked"),
+            relief="flat"
+        )
+        self.button_9.place(
+            x=463.9482421875,
+            y=328.764892578125,
+            width=149.4329833984375,
+            height=49.16864776611328
+        )
+
+    def relative_to_assets(self,path: str) -> Path:
+        return ASSETS_PATH / Path(path)
+
+if __name__ == "__main__":
+    abs = os.getcwd()
+    if(platform.system() == "Darwin"):
+        ASSETS_PATH = abs + "/assets/frame13"
+    else:
+        ASSETS_PATH = abs + "/build/assets/frame13"
+    root = Tk()
+    app = NuovoOrdineMagazzino(root)
+    root.mainloop()
